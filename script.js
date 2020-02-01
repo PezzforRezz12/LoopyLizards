c16d9761fec1a029592ef4bed112b41524945eee
John Marsland <mmmjohnnnyww@gmail.com>
2020-02-01 18:26:54 -0500

    Built Up To This Point

--- script.js
+++ script.js
@@ -1,1 +1,84 @@
-console.log('hello')
+//song 1
+
+const song = [ 
+{name1: 'MACDONALD', action: 'Sally', },
+{animal1: 'cow', action: 'Chicken' },
+{sound1: 'moo', action: 'Cluck' },
+{name2: 'MACDONALD', action: 'Ben' },
+{animal2: 'pig', action: 'Dog' },
+{sound2: 'oink', action: 'Bark' },
+{name3: 'MACDONALD', action: 'Sally' },
+{animal3: 'duck', action: 'Frog' },
+{sound3: 'quack', action: 'Ribbit' },
+]
+
+constol.log(`Old MACDONALD HAD A FARM`)
+
+for (const song of songs){
+
+for (let i = 0; i < 3; i++){
+constol.log(`Old ${song.name1} had a farm E-I-E-I-O`)
+constol.log(`And on his farm he had a ${song.animal1} E-I-E-I-O`)
+constol.log(`With a ${song.name1} ${song.name1} here And a ${song.name1} ${song.name1} there Here a ${song.name1} there a ${song.name1} Everywhere a ${song.name1} ${song.name1}`)
+constol.log(`Old ${song.name1} had a farm E-I-E-I-O`)
+constol.log(`Old ${song.name2} a farm E-I-E-I-O`)
+constol.log(`And on his farm he had a ${song.animal2} E-I-E-I-O`)
+constol.log(`With a ${song.name2} ${song.name2} here And a ${song.name2} ${song.name2} there Here a ${song.name2} there a ${song.name2} Everywhere a ${song.name2} ${song.name2}`)
+constol.log(`Old ${song.name2} had a farm E-I-E-I-O`)
+constol.log(`Old ${song.name3} had a farm E-I-E-I-O`)
+constol.log(`And on his farm he had a ${song.animal3}  E-I-E-I-O`)
+constol.log(`With a ${song.name3} ${song.name3} here And a ${song.name3} ${song.name3} there Here a ${song.name3} there a ${song.name3} Everywhere a ${song.name3} ${song.name3}`)
+constol.log(`Old ${song.name3} had a farm E-I-E-I-O `)
+
+
+}
+
+}
+
+//song 2
+
+console.log(`Bingo was his Name-O.`)
+
+const bing = ['cat','Scard','S-C-A-R-D']
+const word = ['mouse','Apple','A-P-P-L-E'] 
+const flip = ['fish', 'Flood', 'F-L-O-O-D']
+
+for (const i=0; i < bing.length; i++){
+    for (const t=0; t < word.length; t++){
+        for (const v=0; v < flip.length; v++){
+    
+        }
+    }  
+}
+
+consol.log[i](`There was a farmer who had a dog, And Bingo was his name-o.`)
+consol.log[i](`B-I-N-G-O B-I-N-G-O B-I-N-G-O`)
+consol.log[i](`And Bingo was his name-o.`)
+
+consol.log[t](`There was a farmer who had a dog, And Bingo was his name-o.`)
+consol.log[t](`(clap)-I-N-G-O  (clap)-I-N-G-O (clap)-I-N-G-O`)
+consol.log[t](`And Bingo was his name-o.`)
+
+consol.log[v](`There was a farmer who had a dog, And Bingo was his name-o.`)
+consol.log[v](`(clap)-(clap)-N-G-O (clap)-(clap)-N-G-O (clap)-(clap)-N-G-O`)
+consol.log[v](`And Bingo was his name-o.`)
+
+//song 3
+
+
+console.log(`5 Little Ducks `)
+
+const song = {
+    animal1:'Cat',
+    animal2:'kittens',
+    sound: 'meow',
+    action: 'ran',
+}
+    
+consol.log(`5 little ducks went out one day Over the hills and far away Mother duck said “Quack, quack, quack, quack” But only 4 little ducks came back.`)
+consol.log(`4 little ducks went out one day… Over the hills and far away Mother duck said “Quack, quack, quack, quack” But only 3 little ducks came back.`)
+consol.log(`3 little ducks went out one day…Over the hills and far away Mother duck said “Quack, quack, quack, quack” But only 2 little ducks came back.`)
+consol.log(`2 little ducks went out one day… Over the hills and far away Mother duck said “Quack, quack, quack, quack” But only 1 little duck came back.`)
+consol.log(`1 little duck went out one day… Over the hills and far away Mother duck said “Quack, quack, quack, quack” But none of the five little ducks came back`)
+consol.log(`Sad mother duck went out one day… Over the hills and far away Mother duck said “Quack, quack, quack, quack” And all of the five little ducks came back.`)
+
