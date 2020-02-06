18f47c819fab24fe7589b3702fe3022c20499f4a
John Marsland <mmmjohnnnyww@gmail.com>
2020-02-06 16:13:56 -0500

    Final Show

--- script.js
+++ script.js
@@ -1,67 +1,84 @@
 //song 1
 
-const song = [ 
-{name1: 'MACDONALD', action: 'Sally', },
-{animal1: 'cow', action: 'Chicken' },
-{sound1: 'moo', action: 'Cluck' },
-{name2: 'MACDONALD', action: 'Ben' },
-{animal2: 'pig', action: 'Dog' },
-{sound2: 'oink', action: 'Bark' },
-{name3: 'MACDONALD', action: 'Sally' },
-{animal3: 'duck', action: 'Frog' },
-{sound3: 'quack', action: 'Ribbit' },
-]
+const name = [ 'Sally', 'Curtis', 'Benney' ]
+const animal = ['Chicken', 'Duck', 'Goat']
+const sound = ['buck','quack','bahah']
 
-constol.log(`Old MACDONALD HAD A FARM`)
+console.log(`Old MACDONALD HAD A FARM`)
 
-for (const song of songs){
+for (const names of name){
 
-for (let i = 0; i < 3; i++){
-constol.log(`Old ${song.name1} had a farm E-I-E-I-O`)
-constol.log(`And on his farm he had a ${song.animal1} E-I-E-I-O`)
-constol.log(`With a ${song.name1} ${song.name1} here And a ${song.name1} ${song.name1} there Here a ${song.name1} there a ${song.name1} Everywhere a ${song.name1} ${song.name1}`)
-constol.log(`Old ${song.name1} had a farm E-I-E-I-O`)
-constol.log(`Old ${song.name2} a farm E-I-E-I-O`)
-constol.log(`And on his farm he had a ${song.animal2} E-I-E-I-O`)
-constol.log(`With a ${song.name2} ${song.name2} here And a ${song.name2} ${song.name2} there Here a ${song.name2} there a ${song.name2} Everywhere a ${song.name2} ${song.name2}`)
-constol.log(`Old ${song.name2} had a farm E-I-E-I-O`)
-constol.log(`Old ${song.name3} had a farm E-I-E-I-O`)
-constol.log(`And on his farm he had a ${song.animal3}  E-I-E-I-O`)
-constol.log(`With a ${song.name3} ${song.name3} here And a ${song.name3} ${song.name3} there Here a ${song.name3} there a ${song.name3} Everywhere a ${song.name3} ${song.name3}`)
-constol.log(`Old ${song.name3} had a farm E-I-E-I-O `)
-
-
-}
-
-}
-
-//song 2
-
-console.log(`Bingo was his Name-O.`)
-
-const bing = ['cat','Scard','S-C-A-R-D']
-const word = ['mouse','Apple','A-P-P-L-E'] 
-const flip = ['fish', 'Flood', 'F-L-O-O-D']
-
-for (const i=0; i < bing.length; i++){
-    for (const t=0; t < word.length; t++){
-        for (const v=0; v < flip.length; v++){
+for (let i = 0; i < 3 ; i++){
+    console.log(`Old ${name[i]} had a farm E-I-E-I-O
+    And on his farm he had a ${animal[i]} E-I-E-I-O
+    With a ${sound[i]} ${sound[i]} here And a ${sound[i]} ${sound[i]} there Here a ${sound[i]} there a ${sound[i]} Everywhere a ${sound[i]} ${sound[i]}
+    Old ${name[i]} had a farm E-I-E-I-O`)
     
-        }
-    }  
 }
 
-consol.log[i](`There was a farmer who had a dog, And Bingo was his name-o.`)
-consol.log[i](`B-I-N-G-O B-I-N-G-O B-I-N-G-O`)
-consol.log[i](`And Bingo was his name-o.`)
+{
 
-consol.log[t](`There was a farmer who had a dog, And Bingo was his name-o.`)
-consol.log[t](`(clap)-I-N-G-O  (clap)-I-N-G-O (clap)-I-N-G-O`)
-consol.log[t](`And Bingo was his name-o.`)
+}
 
-consol.log[v](`There was a farmer who had a dog, And Bingo was his name-o.`)
-consol.log[v](`(clap)-(clap)-N-G-O (clap)-(clap)-N-G-O (clap)-(clap)-N-G-O`)
-consol.log[v](`And Bingo was his name-o.`)
+// song 2
+
+const bing = ['cat','mouse','fish']
+lyric = ['Scard','Apple','Flood',]
+table = ['S-C-A-R-D', 'A-P-P-L-E', 'F-L-O-O-D']  
+
+
+console.log(`Bingo was his Name-O.`);{
+
+for (const bings of bing)
+for (const lyrics of lyric)
+for (const tables of table)
+for (i = 0; i < 3; i++){    
+console.log(`There was a  who had a. ${bing[i]}, And ${lyric[i]} was his name-o.${table[i]} ${table[i]} And ${lyric[i]} was his name-o.
+There was a farmer who had a dog, And ${lyric[i]} was his name-o.
+(clap)${table[i]}
+(clap)${table[i]}
+(clap)${table[i]}
+And ${lyric[i]} was his name-o.`)
+}
+
+for (i = 0; i < 3; i++){
+console.log(`There was a farmer who had a ${bing[i]},And ${lyric[i]} was his name-o.
+(clap)-(clap)${table[i]}
+(clap)-(clap)${table[i]}
+(clap)-(clap)${table[i]}
+And ${lyric[i]} was his name-o.`)
+}
+for (i = 0; i < 3; i++){ 
+console.log(`There was a farmer who had a ${bing[i]},
+And ${lyric[i]} was his name-o.
+(clap)-(clap)-(clap)${table[i]}
+(clap)-(clap)-(clap)${table[i]}
+(clap)-(clap)-(clap)${table[i]}
+And ${lyric[i]} was his name-o.`)
+}
+for (i = 0; i < 3; i++){ 
+console.log(`There was a farmer who had a ${bing[i]},
+And ${lyric[i]} was his name-o.
+(clap)-(clap)-(clap)-(clap)${table[i]}
+(clap)-(clap)-(clap)-(clap)${table[i]}
+(clap)-(clap)-(clap)-(clap)${table[i]}
+And ${lyric[i]} was his name-o.
+`)
+}
+for (i = 0; i < 3; i++){ 
+console.log(`There was a farmer who had a ${bing[i]},
+And ${lyric[i]} was his name-o.
+(clap)-(clap)-(clap)-(clap)-(clap)
+(clap)-(clap)-(clap)-(clap)-(clap)
+(clap)-(clap)-(clap)-(clap)-(clap)
+And ${lyric[i]} was his name-o.`)
+}
+
+        
+}   
+
+}
+
 
 //song 3
 
@@ -69,16 +86,50 @@ consol.log[v](`And Bingo was his name-o.`)
 console.log(`5 Little Ducks `)
 
 const song = {
-    animal1:'Cat',
-    animal2:'kittens',
-    sound: 'meow',
-    action: 'ran',
+    ducks:'kittens',
+    went:'ran',
+    Mother: 'Cat',
+    quack: 'meow'
 }
+
+
+
+for (const beat in song){
     
-consol.log(`5 little ducks went out one day Over the hills and far away Mother duck said “Quack, quack, quack, quack” But only 4 little ducks came back.`)
-consol.log(`4 little ducks went out one day… Over the hills and far away Mother duck said “Quack, quack, quack, quack” But only 3 little ducks came back.`)
-consol.log(`3 little ducks went out one day…Over the hills and far away Mother duck said “Quack, quack, quack, quack” But only 2 little ducks came back.`)
-consol.log(`2 little ducks went out one day… Over the hills and far away Mother duck said “Quack, quack, quack, quack” But only 1 little duck came back.`)
-consol.log(`1 little duck went out one day… Over the hills and far away Mother duck said “Quack, quack, quack, quack” But none of the five little ducks came back`)
-consol.log(`Sad mother duck went out one day… Over the hills and far away Mother duck said “Quack, quack, quack, quack” And all of the five little ducks came back.`)
+console.log (song[beat])
+for (i = 0; i < 3; i++){
+
+console.log(`5 little ${(song[beat])} ${(song[beat])} out one day
+Over the hills and far away
+${(song[beat])} duck said ${(song[beat])}, ${(song[beat])}, ${(song[beat])}, ${(song[beat])}
+But only 4 little ${(song[beat])} came back.
+
+4 little ${(song[beat])} ${(song[beat])} out one day
+Over the hills and far away
+${(song[beat])} duck said ${(song[beat])}, ${(song[beat])}, ${(song[beat])}, ${(song[beat])}
+But only 4 little ${(song[beat])} came back.
+
+3 little ${(song[beat])} ${(song[beat])} out one day
+Over the hills and far away
+${(song[beat])} duck said ${(song[beat])}, ${(song[beat])}, ${(song[beat])}, ${(song[beat])}
+But only 4 little ${(song[beat])} came back.
+
+2 little ${(song[beat])} ${(song[beat])} out one day
+Over the hills and far away
+${(song[beat])} duck said ${(song[beat])}, ${(song[beat])}, ${(song[beat])}, ${(song[beat])}
+But only 4 little ${(song[beat])} came back.
+
+1 little ${(song[beat])} ${(song[beat])} out one day
+Over the hills and far away
+${(song[beat])} duck said ${(song[beat])}, ${(song[beat])}, ${(song[beat])}, ${(song[beat])}
+But only 4 little ${(song[beat])} came back.
+
+Sad little ${(song[beat])} ${(song[beat])} out one day
+Over the hills and far away
+${(song[beat])} duck said ${(song[beat])}, ${(song[beat])}, ${(song[beat])}, ${(song[beat])}
+But only 4 little ${(song[beat])} came back.`)
+
+}
+
+}
 
